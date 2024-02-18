4db2707daf89f8da63d148e3108f284500924bdf
alief <122020553+liefhax@users.noreply.github.com>
2024-02-18 11:52:36 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,3 +12,41 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+function searchTable() {
+  var input, filter, found, table, tr, td, i, j;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td");
+      for (j = 0; j < td.length; j++) {
+          if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+              found = true;
+          }
+      }
+      if (found) {
+          tr[i].style.display = "";
+          found = false;
+      } else {
+          tr[i].style.display = "none";
+      }
+  }
+}
+
+
+// const filter = document.getElementById("filter");
+// const items = document.querySelectorAll("table tr");
+
+// filter.addEventListener("input", (e) => filterData(e.target.value));
+
+// function filterData(search) {
+//   items.forEach((item) => {
+//     if (item.innerText.toLowerCase().includes(search.toLowerCase())) {
+//       item.classList.remove("d-none");
+//     } else {
+//       item.classList.add("d-none");
+//     }
+//   });
+// }
